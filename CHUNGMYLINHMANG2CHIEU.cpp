6b0221dde7cddec6fc90823e28cc22cc8e89727e
CHUNGMYLINH875 <147479193+CHUNGMYLINH875@users.noreply.github.com>
2023-10-18 10:21:09 +0700

    Add files via upload

---
+++ CHUNGMYLINHMANG2CHIEU.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+ void nhapmang(int*a,int m,int n)
+     {
+     	for(int i=0 ;i<m*n;i++)
+     	  {
+     	  	 printf("\n nhap phan tu thu %d",i);
+     	  	 scanf("%d",(a+i));
+     	  }	
+	 }	      
